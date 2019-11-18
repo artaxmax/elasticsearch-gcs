091d3741b725b650a3355c7e94397d7669525ca5
artaxmax <artaxmax@gmail.com>
2019-11-18 16:53:19 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,12 @@ run_as_other_user_if_needed() {
   else
     exec "${@}"
   fi
-}
 
 [[ -f /usr/share/elasticsearch/config/elasticsearch.keystore ]] || (run_as_other_user_if_needed elasticsearch-keystore create)
 if ! (run_as_other_user_if_needed elasticsearch-keystore list | grep -q '^gcs.client.default.credentials_file$'); then
-    chmod 755 /usr/share/elasticsearch/service-account.json
-    chown -R 1000:0 /usr/share/elasticsearch/service-account.json
-    (run_as_other_user_if_needed elasticsearch-keystore add-file -s 'gcs.client.default.credentials_file' '/usr/share/elasticsearch/service-account.json')
+    chmod 755 /usr/share/elasticsearch/credentials/service-account.json
+    chown -R 1000:0 /usr/share/elasticsearch/credentials/service-account.json
+    (run_as_other_user_if_needed elasticsearch-keystore add-file -s 'gcs.client.default.credentials_file' '/usr/share/elasticsearch/credentials/service-account.json')
 fi
 echo 'elasticsearch plugins:'
 elasticsearch-plugin list
